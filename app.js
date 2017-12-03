d73f0551ca52779e448a819e70a3823d0a7c9709
Ryan Jacobs <ryan.mjacobs@gmail.com>
2017-12-03 06:37:53 -0800

    remove all html from status

--- app.js
+++ app.js
@@ -28,14 +28,14 @@ async function run() {
             let eta    = tds[5];
 
             // clean up status
-            status.replace('<br><font size="1">', '-');
-            status.replace('</font>', '');
+            status.replace(/<(?:.|\n)*?>/gm, "");
 
             // clean up eta
             if (eta.innerHTML.trim().startsWith("<font"))
                 eta = eta.children[0].innerHTML.trim();
             else
                 eta = eta.innerHTML.trim();
+
             if (eta == "&nbsp;")
                 eta = "";
 
