cf1da94241a7381786008cda4ee124059b9a3288
Ryan Jacobs <ryan.mjacobs@gmail.com>
2017-12-09 16:00:16 -0800

    hall=hedrick

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ async function run() {
     const page = await browser.newPage();
 
     await page.goto(
-        "https://www.laundryalert.com/cgi-bin/ucla6023/LMRoom?CallingPage=LMPage&Halls=9");
+        "https://www.laundryalert.com/cgi-bin/ucla6023/LMRoom?CallingPage=LMPage&Halls=8");
 
     let machines = await page.evaluate(() => {
         let machines = [];
